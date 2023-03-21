04a355b040cf5c4e8e5a77e6f8e3adc0a938d9d1
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-21 15:05:44 -0300

    feat: started home page development

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,21 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import { SlidersHorizontal } from "phosphor-react-native";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>
-          Check out some information about your favorites pocket monsters
+        <Text style={styles.headerTitle}>Pokédex</Text>
+        <Text style={styles.headerDescription}>
+          Search for a Pokémon by name or using its National pokedex Number
         </Text>
+      </View>
+      <View style={styles.searchBarContainer}>
+        <TextInput placeholder="Name or number" style={styles.searchBar} placeholderTextColor={'#6C757D'}/>
+        <TouchableOpacity style={styles.filterButton}>
+          <SlidersHorizontal size={35} color="#ffff"/>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,15 +1,50 @@
 import { StyleSheet } from "react-native";
+import { useTheme } from "../../hook/useTheme";
 
+const theme = useTheme();
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#E9ECEF",
+    paddingHorizontal: 18,
   },
+
   header: {
-    paddingTop: 60,
-    paddingHorizontal: 20,
-    alignItems: "center",
+    paddingTop: 70,
+    gap: 6,
   },
-  headerText: {
-    textAlign: "center",
+
+  headerTitle: {
+    fontSize: theme.fontSize.large,
+    fontWeight: "bold",
+    color: theme.colors.TITLE,
+  },
+
+  headerDescription: {
+    fontWeight: "600",
+    color: theme.colors.SUBTITLE,
+    fontSize: theme.fontSize.default,
+  },
+
+  searchBarContainer: {
+    marginTop: 67,
+    flexDirection: "row",
+    gap: 20,
+  },
+
+  searchBar: {
+    backgroundColor: theme.colors.LIGHT_GRAY,
+    padding: 6,
+    borderRadius: 10,
+    width: "85%",
+  },
+
+  filterButton: {
+    width: 37,
+    height: 37,
+    borderRadius: 10,
+    backgroundColor: theme.colors.HARD_GRAY,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
