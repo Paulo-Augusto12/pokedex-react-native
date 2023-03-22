c7a1c48e9e0e099b5a2e82b324d8f10a62feb2df
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-22 11:58:11 -0300

    formatted

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -8,7 +8,7 @@ interface ILoadingComponent {
 export function LoadingComponent({ loading }: ILoadingComponent) {
   if (!loading) return null;
   return (
-    <View style={{padding: 20}}>
+    <View style={{ padding: 20 }}>
       <ActivityIndicator size={35} color="#BAC0D4" />
     </View>
   );
