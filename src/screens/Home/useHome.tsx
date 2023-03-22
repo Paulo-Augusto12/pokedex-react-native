a7bdf95f8c760f733dfc453ee9ab25ace5430990
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-22 11:59:37 -0300

    feat: added correct data states

--- src/screens/Home/useHome.tsx
+++ src/screens/Home/useHome.tsx
@@ -5,6 +5,7 @@ import { Result } from "../../api/DTO/IInitialResultDTO";
 export function useHome() {
   const [pokemons, setPokemons] = useState<IPokemonDTO[]>([]);
   const [pokemonsList, setPokemonstList] = useState<Result[]>([]);
+  const [index, setIndex] = useState(0);
   const [selectedPokemon, setSelectedPokemon] = useState<IPokemonDTO>();
   const [loading, setLoading] = useState(false);
   const [offset, setOffset] = useState(0);
@@ -16,7 +17,16 @@ export function useHome() {
     setLoading(true);
     setOffset(offset + 20);
     const response = await axios.get(baseUrl);
-    setPokemonstList([...pokemonsList, ...response.data.results]);
+
+    const data = response.data.results.map((item: Result) => {
+      return {
+        name: item.name,
+        url: item.url,
+        id: item.url.replace(/[^a-zA-Z0-9 ]/g, "").slice(26),
+      };
+    });
+    setPokemonstList([...pokemonsList, ...data]);
+
     setLoading(false);
   }
 
