38567f4e405b4c1833ea8e908bbf6804bee587f5
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 10:02:17 -0300

    feat: developed initial version of style file

--- src/screens/About_Pokemon/style.ts
+++ src/screens/About_Pokemon/style.ts
@@ -9,5 +9,46 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 18,
   },
 
-  initialText: {},
+  header: {
+    paddingTop: 70,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    gap: 20,
+  },
+  initialDisplay: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  pokemonName: {
+    fontSize: theme.fontSize.large,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  pokemonNationalNumber: {
+    fontSize: theme.fontSize.nationalNumber,
+    color: theme.colors.LIGHT_GRAY,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  pokemonImageContainer: {
+    width: 324,
+    height: 357,
+    marginTop: 60,
+    backgroundColor: "#CFD4BA",
+    alignSelf: "center",
+    borderRadius: 10,
+    padding: 6,
+  },
+  pokemonImage: {
+    width: 312,
+    height: 345,
+  },
+
+  actionMenu: {
+    paddingTop: 41,
+    alignSelf: "center",
+  },
 });
