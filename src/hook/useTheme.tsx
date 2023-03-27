ff3a38a671ea6239b2c04ee9c430ac8401f528ee
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 11:30:58 -0300

    feat: added type color to hook theme

--- src/hook/useTheme.tsx
+++ src/hook/useTheme.tsx
@@ -14,9 +14,32 @@ export function useTheme() {
     default: 13,
     nationalNumber: 26,
   };
+  const colorTypes = {
+    normal: "#D3D3D3",
+    fighting: "#ffb703",
+    flying: "#8ecae6",
+    poison: "#0f4c5c",
+    ground: "#9d0208",
+    rock: "#6f1d1b",
+    bug: "#e0c045",
+    ghost: "#191528",
+    steel: "#a4c3b2",
+    fire: "#c1121f",
+    water: "#219ebc",
+    grass: "#55a630",
+    electric: "#ffff3f",
+    psychic: "#bde0fe",
+    ice: "#7bdff2",
+    dragon: "#08415c",
+    dark: "#1b1f22",
+    fairy: "#ffafcc",
+    unknown: "#1b2021",
+    shadow: "#291720",
+  };
 
   return {
     colors,
     fontSize,
+    colorTypes,
   };
 }
