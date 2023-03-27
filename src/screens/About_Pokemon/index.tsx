c17a63770b2aa410c15259447625a80595abeb28
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 14:23:16 -0300

    feat: changed view hierarchy

--- src/screens/About_Pokemon/index.tsx
+++ src/screens/About_Pokemon/index.tsx
@@ -1,77 +1,118 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import { styles } from "./style";
 import { useAboutPokemon } from "./useAbout_Pokemon";
 import { ArrowLeft } from "phosphor-react-native";
 import { Props } from "../../types/Routes";
+import { useTheme } from "../../hook/useTheme";
+import { LoadingComponent } from "../../components/Loading";
 
 export function AboutPokemon({ navigation, route }: Props) {
   const hook = useAboutPokemon();
+
+  useEffect(() => {
+    hook.handleGetPokemonData(route.params.pokemonName);
+  }, [hook.pokemonData]);
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.initialDisplay}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <View style={{ width: "10%" }}>
-              <ArrowLeft size={32} />
-            </View>
-          </TouchableOpacity>
-          <View style={{ width: "90%" }}>
-            <Text style={styles.pokemonName}>
-              {route.params.pokemonName[0].toUpperCase() +
-                route.params.pokemonName.substring(1)}
-            </Text>
-          </View>
+      {!route.params.pokemonName.trim() ? (
+        <View>
+          <LoadingComponent loading={!route.params.pokemonName.trim()} />
         </View>
-
-        {route.params.pokemonNumber < 10 ? (
-          <Text style={styles.pokemonNationalNumber}>
-            00{route.params.pokemonNumber}
-          </Text>
-        ) : route.params.pokemonNumber < 100 ? (
-          <Text style={styles.pokemonNationalNumber}>
-            0{route.params.pokemonNumber}
-          </Text>
-        ) : (
-          <Text style={styles.pokemonNationalNumber}>
-            {route.params.pokemonNumber}
-          </Text>
-        )}
-      </View>
-      <View style={styles.pokemonImageContainer}>
-        <Image
-          source={{
-            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${route.params.pokemonNumber}.png`,
-          }}
-          style={styles.pokemonImage}
-        />
-      </View>
-      <View style={styles.actionMenu}>
-        <FlatList
-          horizontal
-          ItemSeparatorComponent={() => <View style={{ padding: 15 }} />}
-          data={hook.menuActionTags}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => hook.setSelectedTag(item.id)}
+      ) : (
+        <>
+          {!hook.backgroundTypeColor.color.trim() ? (
+            <View>
+              <LoadingComponent loading={!route.params.pokemonName.trim()} />
+            </View>
+          ) : (
+            <View
+              style={[
+                styles.pokemonImageContainer,
+                { backgroundColor: hook.backgroundTypeColor.color },
+              ]}
             >
-              <View>
-                <Text
-                  style={{
-                    color: hook.selectedTag === item.id ? "#000000" : "#6C757D",
-                    fontSize: 16,
-                    fontWeight: hook.selectedTag === item.id ? "bold" : "600",
-                  }}
-                >
-                  {item.tagName}
-                </Text>
+              <View style={styles.header}>
+                <View style={styles.initialDisplay}>
+                  <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <View style={{ width: "10%" }}>
+                      <ArrowLeft
+                        size={32}
+                        color={hook.backgroundTypeColor.nameColor}
+                      />
+                    </View>
+                  </TouchableOpacity>
+                  <View style={{ width: "90%" }}>
+                    <Text
+                      style={[
+                        styles.pokemonName,
+                        { color: hook.backgroundTypeColor.nameColor },
+                      ]}
+                    >
+                      {route.params.pokemonName[0].toUpperCase() +
+                        route.params.pokemonName.substring(1)}
+                    </Text>
+                  </View>
+                </View>
+
+                {route.params.pokemonNumber < 10 ? (
+                  <Text
+                    style={[
+                      styles.pokemonNationalNumber,
+                      { color: hook.backgroundTypeColor.nationalNumberColor },
+                    ]}
+                  >
+                    00{route.params.pokemonNumber}
+                  </Text>
+                ) : route.params.pokemonNumber < 100 ? (
+                  <Text style={styles.pokemonNationalNumber}>
+                    0{route.params.pokemonNumber}
+                  </Text>
+                ) : (
+                  <Text style={styles.pokemonNationalNumber}>
+                    {route.params.pokemonNumber}
+                  </Text>
+                )}
               </View>
-            </TouchableOpacity>
+
+              <Image
+                source={{
+                  uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${route.params.pokemonNumber}.png`,
+                }}
+                style={styles.pokemonImage}
+              />
+            </View>
           )}
-          showsHorizontalScrollIndicator={false}
-        />
-      </View>
+          <View style={styles.actionMenu}>
+            <FlatList
+              horizontal
+              ItemSeparatorComponent={() => <View style={{ padding: 15 }} />}
+              data={hook.menuActionTags}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  key={item.id}
+                  onPress={() => hook.setSelectedTag(item.id)}
+                >
+                  <View>
+                    <Text
+                      style={{
+                        color:
+                          hook.selectedTag === item.id ? "#000000" : "#6C757D",
+                        fontSize: 16,
+                        fontWeight:
+                          hook.selectedTag === item.id ? "bold" : "600",
+                      }}
+                    >
+                      {item.tagName}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+              showsHorizontalScrollIndicator={false}
+            />
+          </View>
+        </>
+      )}
     </View>
   );
 }
