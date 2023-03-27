5d81de035d54ef6834e57b38f6b93c3b274be15b
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 12:00:16 -0300

    feat: making axios request on hook

--- src/screens/About_Pokemon/useAbout_Pokemon.tsx
+++ src/screens/About_Pokemon/useAbout_Pokemon.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { IPokemonDTO } from "../../api/DTO/IPokemonDTO";
 
 export function useAboutPokemon() {
   const [selectedTag, setSelectedTag] = useState(1);
+  const [pokemonData, setPokemonData] = useState<IPokemonDTO>();
   const menuActionTags = [
     {
       tagName: "Abilities",
@@ -25,9 +28,20 @@ export function useAboutPokemon() {
     },
   ];
 
+  async function handleGetPokemonData(name: string) {
+    const baseUrl = `https://pokeapi.co/api/v2/pokemon/${name}`;
+    const response = await axios.get(baseUrl);
+    setPokemonData(response.data);
+
+    console.log(pokemonData, "aquiii");
+  }
+
   return {
     menuActionTags,
     selectedTag,
     setSelectedTag,
+    handleGetPokemonData,
+    pokemonData,
+    setPokemonData,
   };
 }
