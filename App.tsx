33013b288583df5fef0a7d0d2f2dade30e5ff34b
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 09:22:39 -0300

    feat: provisionally switched display page for quicker development

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/screens/Home";
+import { AboutPokemon } from "./src/screens/About_Pokemon";
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
-      <Home />
+      <AboutPokemon />
     </>
   );
 }
