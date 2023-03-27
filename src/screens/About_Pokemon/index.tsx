73bb78249fb4a16eb1b26b36e20dd5e14f72da11
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 10:02:09 -0300

    feat: developed initial version of index file

--- src/screens/About_Pokemon/index.tsx
+++ src/screens/About_Pokemon/index.tsx
@@ -1,12 +1,59 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
-import { styles } from './style'
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import { styles } from "./style";
+import { useAboutPokemon } from "./useAbout_Pokemon";
+import { ArrowLeft } from "phosphor-react-native";
 
 export function AboutPokemon() {
+  const hook = useAboutPokemon();
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <View>About Pokemon</View>
+      <View style={styles.header}>
+        <View style={styles.initialDisplay}>
+          <TouchableOpacity>
+            <View style={{ width: "10%" }}>
+              <ArrowLeft size={32} />
+            </View>
+          </TouchableOpacity>
+          <View style={{ width: "90%" }}>
+            <Text style={styles.pokemonName}>Bulbasaur</Text>
+          </View>
+        </View>
+        <Text style={styles.pokemonNationalNumber}>001</Text>
+      </View>
+      <View style={styles.pokemonImageContainer}>
+        <Image
+          source={{
+            uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+          }}
+          style={styles.pokemonImage}
+        />
+      </View>
+      <View style={styles.actionMenu}>
+        <FlatList
+          horizontal
+          ItemSeparatorComponent={() => <View style={{ padding: 15 }} />}
+          data={hook.menuActionTags}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              key={item.id}
+              onPress={() => hook.setSelectedTag(item.id)}
+            >
+              <View>
+                <Text
+                  style={{
+                    color: hook.selectedTag === item.id ? "#000000" : "#6C757D",
+                    fontSize: 16,
+                    fontWeight: "600",
+                  }}
+                >
+                  {item.tagName}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
 }
