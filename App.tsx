edc35c28e29363712446d626fb370259f52e82cb
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 10:16:53 -0300

    feat: developed navigation container on app for screen navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/screens/Home";
 import { AboutPokemon } from "./src/screens/About_Pokemon";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Navigation = createNativeStackNavigator();
   return (
-    <>
+    <NavigationContainer>
       <StatusBar style="dark" />
-      <AboutPokemon />
-    </>
+      <Navigation.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="home"
+      >
+        <Navigation.Screen name={"home"} component={Home} />
+        <Navigation.Screen name={"about"} component={AboutPokemon} />
+      </Navigation.Navigator>
+    </NavigationContainer>
   );
 }
