6073e94bb07be313484989336b250d3034b6df8d
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-27 09:07:29 -0300

    feat: started about pokemon page index file development

--- src/screens/About_Pokemon/index.tsx
+++ src/screens/About_Pokemon/index.tsx
@@ -1,2 +1,12 @@
 import React from "react";
-import { View, Text,  } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { styles } from './style'
+
+export function AboutPokemon() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+      <View>About Pokemon</View>
+    </View>
+  );
+}
