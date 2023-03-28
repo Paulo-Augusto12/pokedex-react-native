194ec72d40223e9f5d16a59eb6568583254601d4
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-28 11:17:46 -0300

    feat: developed initial version of ability card container style file

---
+++ src/components/AbilityCard/style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+import { useTheme } from "../../hook/useTheme";
+
+const theme = useTheme();
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    gap: 12,
+  },
+  AbilityNameWrapper: {
+    width: "100%",
+    textAlign: "left",
+  },
+  AbilityName: {
+    color: theme.colors.TITLE,
+    fontSize: 20,
+  },
+  abilityContainer: {
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 12,
+  },
+  abilityDescription: {
+    fontSize: 20,
+    fontWeight: '600'
+  },
+});
